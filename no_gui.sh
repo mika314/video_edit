6ed333d434fc31abf234ef8f7e954c43bc9cf2b2
Anton Te <te.anton@gmail.com>
2014-03-20 19:50:25 -0700

    Correct the length of the jingle

--- no_gui.sh
+++ no_gui.sh
@@ -4,7 +4,7 @@ rootdir=`dirname $0`
 
 convert ~/dl/lmte.png -fill blue -font DejaVu-Sans-Mono-Book -pointsize 50 -draw "text 80,640 '$2'" `basename $1 .mp4`.png
 $rootdir/silence_detector/silence_detector $1
-avconv -i $1 -f yuv4mpegpipe - | $rootdir/frame_remover/frame_remover $1_rm.txt 48000 | $rootdir/prepend_frame/prepend_frame `basename $1 .mp4`.png 10000 | yuvview | avconv -f yuv4mpegpipe -i - -b 10000k -threads 8 `basename $1 .mp4`_noaudio.mp4
+avconv -i $1 -f yuv4mpegpipe - | $rootdir/frame_remover/frame_remover $1_rm.txt 48000 | $rootdir/prepend_frame/prepend_frame `basename $1 .mp4`.png 9188 | yuvview | avconv -f yuv4mpegpipe -i - -b 10000k -threads 8 `basename $1 .mp4`_noaudio.mp4
 mv $1.s16l $1.raw
 sox -r 48k -e signed -b 16 -c 1 $1.raw $1_compressed.raw compand 0.3,1 6:-55,-50,-20 -5 -90 0.2
 $rootdir/add_bg/add_bg $1_compressed.raw ~/dl/Microchip.raw > $1_bg.raw
